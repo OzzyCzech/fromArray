05da4402a52e920b30ccc6c4d74a5db29980b007
Tarık Filiz <tarikfiliz@icloud.com>
2025-04-08 11:22:48 +0000

    scheme now supports array of classes

--- src/FromArray.php
+++ src/FromArray.php
@@ -7,58 +7,70 @@ namespace DataLoader;
  *
  * @package DataLoader
  */
-trait FromArray {
+trait FromArray
+{
 
-	public static function fromArray(array $data = [], ?callable $filter = null, array $scheme = [], array $mapping = []): static {
-		$class = get_called_class();
+  public static function fromArray(array $data = [], ?callable $filter = null, array $scheme = [], array $mapping = []): static
+  {
+    $class = get_called_class();
 
-		// Allow to define schema in class
-		if (defined(constant_name: "$class::SCHEME")) {
-			$scheme = array_merge($class::SCHEME, $scheme);
-		}
+    // Allow to define schema in class
+    if (defined(constant_name: "$class::SCHEME")) {
+      $scheme = array_merge($class::SCHEME, $scheme);
+    }
 
-		// Allow map one property to another
-		if (defined(constant_name: "$class::MAPPING")) {
-			$mapping = array_flip(array_merge($class::MAPPING, $mapping));
-		}
+    // Allow map one property to another
+    if (defined(constant_name: "$class::MAPPING")) {
+      $mapping = array_flip(array_merge($class::MAPPING, $mapping));
+    }
 
-		// Hydrate object with values
-		foreach (get_object_vars($obj = new $class) as $property => $default) {
+    // Hydrate object with values
+    foreach (get_object_vars($obj = new $class) as $property => $default) {
 
-			// Resolve data key with mapping array
-			$key = array_key_exists($property, $mapping) ? $mapping[$property] : $property;
+      // Resolve data key with mapping array
+      $key = array_key_exists($property, $mapping) ? $mapping[$property] : $property;
 
-			// Skip missing data
-			if (!array_key_exists($key, $data)) continue;
+      // Skip missing data
+      if (!array_key_exists($key, $data)) continue;
 
-			// Filter values with callback
-			$value = is_callable($filter) ? call_user_func($filter, $data[$key], $property, $default) : $data[$key];
+      // Filter values with callback
+      $value = is_callable($filter) ? call_user_func($filter, $data[$key], $property, $default) : $data[$key];
 
-			// Solving scheme prescription...
-			if (array_key_exists($property, $scheme)) {
+      // Solving scheme prescription...
+      if (array_key_exists($property, $scheme)) {
 
-				// 1. Scheme prescription is Class
-				if (is_string($scheme[$property]) && class_exists($scheme[$property])) {
+        // 1. Scheme prescription is Class
+        if (is_string($scheme[$property]) && class_exists($scheme[$property])) {
 
-					if (method_exists($scheme[$property], __FUNCTION__)) {
-						// 1.1 fromArray() method exists
-						$obj->{$property} = call_user_func([$scheme[$property], __FUNCTION__], (array) $value, $filter);
-					} else {
-						// 1.2 Create object with constructor
-						$obj->{$property} = new $scheme[$property]($value);
-					}
-				}
+          $obj->{$property} = self::processNestedValue($value, $scheme[$property], $filter);
+        }
 
-				// 2. Scheme prescription is Callback e.g. strval, intval, function ($value) {}
-				if (is_callable($scheme[$property])) {
-					$obj->{$property} = call_user_func($scheme[$property], $value);
-				}
+        // 2. Scheme prescription is Callback e.g. strval, intval, function ($value) {}
+        elseif (is_callable($scheme[$property])) {
+          $obj->{$property} = call_user_func($scheme[$property], $value);
+        } else {
+          throw new \InvalidArgumentException("Invalid SCHEME definition for property {$property}");
+        }
+      } else {
+        $obj->{$property} = $value; // assign value to object
+      }
+    }
 
-			} else {
-				$obj->{$property} = $value; // assign value to object
-			}
-		}
+    return $obj;
+  }
 
-		return $obj;
-	}
+  private static function processNestedValue(mixed $value, string $class, ?callable $filter): mixed
+  {
+    if (is_array($value)) {
+      if (array_is_list($value)) {
+        // Handle array of nested class instances
+        return array_map(fn($item) => self::processNestedValue($item, $class, $filter), $value);
+      } elseif (method_exists($class, 'fromArray')) {
+        // Handle single nested class instance
+        return call_user_func([$class, 'fromArray'], $value, $filter);
+      }
+    }
+    // If it's not an array, assume it's a direct instantiation
+    return new $class($value);
+  }
 }
