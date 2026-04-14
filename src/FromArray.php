f6c82c512817ce75f44d26ed1c49a713d5c0e992
Roman Ožana <roman@ozana.cz>
2026-04-14 12:16:08 +0200

    refactor: replace get_called_class() with static::class

--- src/FromArray.php
+++ src/FromArray.php
@@ -11,10 +11,8 @@ trait FromArray
 {
     public static function fromArray(array $data = []): static
     {
-        $class = get_called_class();
-
-        $object = new $class();
-        foreach (Metadata::getProperties($class) as $property) {
+        $object = new static();
+        foreach (Metadata::getProperties(static::class) as $property) {
             $key = $property->from ?? $property->name;
 
             if (!array_key_exists($key, $data)) {
